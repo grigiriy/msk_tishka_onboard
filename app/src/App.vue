91c3cdc45d135e159279c80e0088dc9929e17c71
Grigiriy <grigiriy.malyshev@gmail.com>
2024-08-26 14:33:36 +0300

    v 0.04 test form

--- app/src/App.vue
+++ app/src/App.vue
@@ -6,17 +6,14 @@
 import './styles.scss';
 
 import { Root } from '@tok/generation';
-
-if(
-  !Telegram.WebApp.initDataUnsafe.user
-){return false}
 </script>
 
 <style>
 html[data-theme="dark"] body {
   background-color:rgb(15 15 16)!important;
 }
-input:focus {
+input:focus,
+textarea:focus {
   outline: none;
 }
 </style>
--- app/src/config.ts
+++ app/src/config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from '@tok/generation';
 
 
 
-import ActionSlide from './custom/components/ConstructorSection.vue';
+import ConstructorSection from './custom/ConstructorSection.vue';
+import OrderSection from './custom/OrderSection.vue';
 
 const imageStyle =
   'left: 50%; top: 50%; transform: translate(-50%, -50%); max-width: initial';
@@ -10,16 +11,45 @@ const imageStyle =
 
 export default defineConfig({
   definePresets: {
-    action_slide: ActionSlide,
+    constructor_slide: ConstructorSection,
+    order_slide: OrderSection,
   },
   pages: [
     {
       slides: [
-        // intro
         {
-          extends: 'action_slide',
-          button: 'Перейти к оформлению',
+          // extends: 'order_slide',
+          // button: 'Оформить заказ',
+          extends: 'order_slide',
+          media: {
+            type: 'sticker',
+            src: import('./assets/stickers/duck_spy.tgs'),
+            size: 150,
+          },
+          shape: 'square',
+          pagination: 'count',
+          title: 'Forms',
+          description: 'User fills in the form – the bot receives the data',
+          form: [
+            {
+              id: 'text_from_form',
+              placeholder: 'Text input',
+              type: 'text',
+            },
+            {
+              id: 'number_from_form',
+              placeholder: 'Number input',
+              type: 'number',
+            },
+            {
+              id: 'checkbox_from_form',
+              placeholder: 'Checkbox',
+              type: 'checkbox',
+            },
+          ],
+          button: 'Next',
         },
+        // intro
         {
           media: {
             type: 'sticker',
@@ -32,26 +62,7 @@ export default defineConfig({
           title: 'Привет!',
           description:"Я бот, который умеет делать (почти) любые принты на одежде.<br><br>Сейчас у меня есть футболки, свитшоты (это толстовка без капюшона) и худи (это толстовка с капюшоном). И два базовых цвета: черный и белый.<br><br>И любые размеры. От xs-феечек до xxxl-богатырей. Помни, что все мои вещи — это небольшой оверсайз.",
           button: 'Ясно',
-
-          // title: 'Привет!',
-          // description:"Сейчас расскажу, как здесь все работает:<br>Грузи в меня картинки, и выбирай модель одежды. Об оплате поговорим на последнем шаге)",
-          // button: 'Круто! дальше',
         },
-
-        // image
-        // {
-        //   media: {
-        //     type: 'image',
-        //     src: import('./assets/img/chick.jpg'),
-        //   },
-        //   shape: 'rounded',
-        //   pagination: 'count',
-        //   title: 'Что я могу',
-        //   description:"Я могу делать принты на футболках, свитшотах (толстовка без капюшона) и худи (толстовка с капюшоном).<br><br>У меня (пока что) есть два базовых цвета: черный и белый.<br><br>И множество размеров от xs до 3xl",
-        //   button: 'Пон, дальше',
-        // },
-
-        // sticker
         {
           media: {
             type: 'sticker',
@@ -64,85 +75,84 @@ export default defineConfig({
           title: 'Есть ограничения!',
           description:'Я могу отказать в печати, если твоя картинка нарушает какие-нибудь законы или моё чувство прекрасного.<br><br>Боту, конечно, ничего не будет, а вот кожаным, которые будут это печатать, может и прилететь. Так что сегодня без фурри-порно, прости.<br><br>Если у тебя специфический заказ, пиши вот сюда <a href="https://t.me/gmalyshev">@gmalyshev</a> — обсудим лично.',
           button: 'Понятно',
-
-          // description: 'Я могу отказать в печати по личным причинам, например, всякие БЛМ или прон я печатать не буду (либо за сильно дорохо - можем покумекать ЛС)',
-          // button: 'Пон, согл',
         },
-
-        // sticker
         {
           media: {
             type: 'image',
-            src: import('./assets/img/go.webp'),
+            src: import('./assets/img/chick.jpg'),
           },
           shape: 'square',
           pagination: 'count',
 
-          title: 'Как заказать?',
+          title: 'Как это работает?',
           description:'Всё очень просто. Кидай в меня картинку, которую хочешь напечатать, выбирай модель одежды, размер и цвет. И оплачивай заказ.<br><br>Срок изготовления – 1 (иногда 2) дня с момента оплаты. Статус заказа я буду присылать тебе в нашу переписку.<br><br>Доставка — по всей России СДЭКом или Почтой, но оплачиваешь её ты — при получении.<br><br>Если остались вопросы, пиши <a href="https://t.me/gmalyshev">@gmalyshev</a>.<br><br><b style="font-size:24px">Погнали?</b><br><br>',
           button: 'ПОГНАЛИ!',
-          
-          // title: 'Крч,',
-          // description:'делай десигн, выбирай размеры итп<br>Как будешь готов, жми "КУПИТЬ", и я скину тебе куар для оплаты.<br><br><b style="font-size:24px">Погнали?</b>',
-          // button: 'ПОГНАЛИ!',
+        },
+        {
+          extends: 'constructor_slide',
+          button: 'Заказать!',
+        },
+        {
+          extends: 'order_slide',
+          button: 'Оформить заказ',
         },
       ],
     },
 
     // paywall
-    {
-      extends: 'paywall',
-      path: '/paywall',
-      media: {
-        type: 'sticker',
-        src: import('./assets/stickers/duck_money.tgs'),
-        size: 150,
-      },
-      shape: 'square',
-      title: 'Your beautiful Paywall',
-      list: [
-        'Adjustable product cards',
-        '<b>👛 Wallet Pay</b> and <b>Telegram Payments</b> ready. Add custom methods easily',
-        'Subscriptions or One-time payments',
-      ],
-      products: [
-        {
-          id: '1_month_subscription',
-          title: '1 month subscription',
-          description: '2$/month',
-          discount: '',
-          price: 2,
-        },
-        {
-          id: '1_year_subscription',
-          title: '1 year subscription',
-          description: '1$/month',
-          discount: 'Discount 50%',
-          price: 12,
-        },
-        {
-          id: 'lifetime_access',
-          title: 'Lifetime access',
-          description: '20$ once',
-          discount: 'Best offer',
-          price: 20,
-        },
-      ],
-      mainButtonText: 'Buy for {price}',
-      popup: {
-        // popup for payment methods choice
-        type: 'web',
-      },
-      links: [
-        {
-          text: 'Privacy policy',
-          href: 'https://google.com',
-        },
-        {
-          text: 'Terms of use',
-          href: 'https://google.com',
-        },
-      ],
-    },
+    // {
+    //   extends: 'paywall',
+    //   path: '/paywall',
+    //   media: {
+    //     type: 'sticker',
+    //     src: import('./assets/stickers/duck_money.tgs'),
+    //     size: 150,
+    //   },
+    //   shape: 'square',
+    //   title: 'Your beautiful Paywall',
+    //   list: [
+    //     'Adjustable product cards',
+    //     '<b>👛 Wallet Pay</b> and <b>Telegram Payments</b> ready. Add custom methods easily',
+    //     'Subscriptions or One-time payments',
+    //   ],
+    //   products: [
+    //     {
+    //       id: '1_month_subscription',
+    //       title: '1 month subscription',
+    //       description: '2$/month',
+    //       discount: '',
+    //       price: 2,
+    //     },
+    //     {
+    //       id: '1_year_subscription',
+    //       title: '1 year subscription',
+    //       description: '1$/month',
+    //       discount: 'Discount 50%',
+    //       price: 12,
+    //     },
+    //     {
+    //       id: 'lifetime_access',
+    //       title: 'Lifetime access',
+    //       description: '20$ once',
+    //       discount: 'Best offer',
+    //       price: 20,
+    //     },
+    //   ],
+    //   mainButtonText: 'Buy for {price}',
+    //   popup: {
+    //     // popup for payment methods choice
+    //     type: 'web',
+    //   },
+    //   links: [
+    //     {
+    //       text: 'Privacy policy',
+    //       href: 'https://google.com',
+    //     },
+    //     {
+    //       text: 'Terms of use',
+    //       href: 'https://google.com',
+    //     },
+    //   ],
+    // },
   ],
 });
